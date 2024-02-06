0a6a338619d61f98be8aed93f99f5139cbe734ec
peter <80846730+unknownpedro@users.noreply.github.com>
2024-02-06 16:44:46 -0300

    Update fletApp.py

--- fletApp.py
+++ fletApp.py
@@ -1,17 +1,20 @@
 import flet as ft
 import calculator as cal
 
-num = 0
-type = str
+
 def main(page: ft.Page):
     page.title = "Calculator"
     page.vertical_alignment = ft.MainAxisAlignment.CENTER
 
-    txt_number = ft.TextField(value="0", text_align=ft.TextAlign.RIGHT, width=280)
+    txt_number = ft.TextField(value="0", text_align=ft.TextAlign.RIGHT, width=330)
+
+    num = 0
+    tipo = ""
+    total = 0
 
     def num_one(e):
         txt_number.value = int(txt_number.value)
-        if (txt_number.value == 0):
+        if txt_number.value == 0:
             txt_number.value = 1
         else:
             txt_number.value = str(txt_number.value)
@@ -20,7 +23,7 @@ def main(page: ft.Page):
 
     def num_two(e):
         txt_number.value = int(txt_number.value)
-        if (txt_number.value == 0):
+        if txt_number.value == 0:
             txt_number.value = 2
         else:
             txt_number.value = str(txt_number.value)
@@ -29,7 +32,7 @@ def main(page: ft.Page):
 
     def num_three(e):
         txt_number.value = int(txt_number.value)
-        if (txt_number.value == 0):
+        if txt_number.value == 0:
             txt_number.value = 3
         else:
             txt_number.value = str(txt_number.value)
@@ -38,7 +41,7 @@ def main(page: ft.Page):
 
     def num_four(e):
         txt_number.value = int(txt_number.value)
-        if (txt_number.value == 0):
+        if txt_number.value == 0:
             txt_number.value = 4
         else:
             txt_number.value = str(txt_number.value)
@@ -47,7 +50,7 @@ def main(page: ft.Page):
 
     def num_five(e):
         txt_number.value = int(txt_number.value)
-        if (txt_number.value == 0):
+        if txt_number.value == 0:
             txt_number.value = 5
         else:
             txt_number.value = str(txt_number.value)
@@ -56,7 +59,7 @@ def main(page: ft.Page):
 
     def num_six(e):
         txt_number.value = int(txt_number.value)
-        if (txt_number.value == 0):
+        if txt_number.value == 0:
             txt_number.value = 6
         else:
             txt_number.value = str(txt_number.value)
@@ -65,7 +68,7 @@ def main(page: ft.Page):
 
     def num_seven(e):
         txt_number.value = int(txt_number.value)
-        if (txt_number.value == 0):
+        if txt_number.value == 0:
             txt_number.value = 7
         else:
             txt_number.value = str(txt_number.value)
@@ -74,7 +77,7 @@ def main(page: ft.Page):
 
     def num_eight(e):
         txt_number.value = int(txt_number.value)
-        if (txt_number.value == 0):
+        if txt_number.value == 0:
             txt_number.value = 8
         else:
             txt_number.value = str(txt_number.value)
@@ -83,7 +86,7 @@ def main(page: ft.Page):
 
     def num_nine(e):
         txt_number.value = int(txt_number.value)
-        if (txt_number.value == 0):
+        if txt_number.value == 0:
             txt_number.value = 9
         else:
             txt_number.value = str(txt_number.value)
@@ -92,14 +95,14 @@ def main(page: ft.Page):
 
     def num_zero(e):
         txt_number.value = int(txt_number.value)
-        if (txt_number.value == 0):
+        if txt_number.value == 0:
             txt_number.value = 0
         else:
             txt_number.value = str(txt_number.value)
             txt_number.value = txt_number.value + "0"
         page.update()
 
-    def clearall(e):
+    def clearer(e):
         txt_number.value = int(txt_number.value)
         txt_number.value = 0
         txt_number.value = str(txt_number.value)
@@ -109,41 +112,62 @@ def main(page: ft.Page):
         txt_number.value = txt_number.value[:-1]
         page.update()
 
-    def actionadd(e):
-        type = "so"
+    def actadd(e):
+        nonlocal num, tipo
+        tipo = "so"
         txt_number.value = int(txt_number.value)
         num = int(txt_number.value)
         txt_number.value = 0
         txt_number.value = str(txt_number.value)
         page.update()
 
-    def actionsub(e):
-        type = "su"
+    def actsub(e):
+        nonlocal num, tipo
+        tipo = "su"
         txt_number.value = int(txt_number.value)
         num = int(txt_number.value)
         txt_number.value = 0
         txt_number.value = str(txt_number.value)
         page.update()
 
-    def actionmult(e):
-        type = "mu"
+    def actmult(e):
+        nonlocal num, tipo
+        tipo = "mu"
         txt_number.value = int(txt_number.value)
         num = int(txt_number.value)
         txt_number.value = 0
         txt_number.value = str(txt_number.value)
         page.update()
 
-    def actiondiv(e):
-        type = "di"
+    def actdiv(e):
+        nonlocal num, tipo
+        tipo = "di"
         txt_number.value = int(txt_number.value)
         num = int(txt_number.value)
         txt_number.value = 0
         txt_number.value = str(txt_number.value)
         page.update()
 
-    def rotina():
-
+    def igual(e):
+        nonlocal num, tipo, total
+        num2 = float(txt_number.value)
+        if tipo == "so":
+            total = cal.add(num, num2)
+        elif tipo == "su":
+            total = cal.sub(num, num2)
+        elif tipo == "mu":
+            total = cal.multi(num, num2)
+        elif tipo == "di":
+            total = cal.div(num, num2)
+        else:
+            print("Operação desconhecida:", tipo)
+        txt_number.value = str(total)
         page.update()
+
+        num = total
+        tipo = ""
+        total = 0
+        txt_number.value = 0
 
     page.add(
         ft.Row(
@@ -154,46 +178,47 @@ def main(page: ft.Page):
         ),
         ft.Row(
             [
-                ft.FilledTonalButton(text="7", on_click=num_seven),
-                ft.FilledTonalButton(text="8", on_click=num_eight),
-                ft.FilledTonalButton(text="9", on_click=num_nine),
-                ft.FilledTonalButton(text="C", on_click=clear),
+                ft.FilledTonalButton(text="7", on_click=num_seven, width=75),
+                ft.FilledTonalButton(text="8", on_click=num_eight, width=75),
+                ft.FilledTonalButton(text="9", on_click=num_nine, width=75),
+                ft.FilledTonalButton(text="C", on_click=clear, width=75),
             ],
             alignment=ft.MainAxisAlignment.CENTER,
         ),
         ft.Row(
             [
-                ft.FilledTonalButton(text="4", on_click=num_four),
-                ft.FilledTonalButton(text="5", on_click=num_five),
-                ft.FilledTonalButton(text="6", on_click=num_six),
-                ft.FilledTonalButton(text="+", on_click=actionadd),
+                ft.FilledTonalButton(text="4", on_click=num_four, width=75),
+                ft.FilledTonalButton(text="5", on_click=num_five, width=75),
+                ft.FilledTonalButton(text="6", on_click=num_six, width=75),
+                ft.FilledTonalButton(text="+", on_click=actadd, width=75),
             ],
             alignment=ft.MainAxisAlignment.CENTER,
         ),
         ft.Row(
             [
-                ft.FilledTonalButton(text="1", on_click=num_one),
-                ft.FilledTonalButton(text="2", on_click=num_two),
-                ft.FilledTonalButton(text="3", on_click=num_three),
-                ft.FilledTonalButton(text="-", on_click=actionsub),
+                ft.FilledTonalButton(text="1", on_click=num_one, width=75),
+                ft.FilledTonalButton(text="2", on_click=num_two, width=75),
+                ft.FilledTonalButton(text="3", on_click=num_three, width=75),
+                ft.FilledTonalButton(text="-", on_click=actsub, width=75),
             ],
             alignment=ft.MainAxisAlignment.CENTER,
         ),
-    ft.Row(
-        [
-            ft.FilledTonalButton(text="0", on_click=num_zero),
-            ft.FilledTonalButton(text="CA", on_click=clearall),
-            ft.FilledTonalButton(text="/", on_click=actiondiv),
-            ft.FilledTonalButton(text="*", on_click=actionmult),
-        ],
-        alignment=ft.MainAxisAlignment.CENTER,
+        ft.Row(
+            [
+                ft.FilledTonalButton(text="0", on_click=num_zero, width=75),
+                ft.FilledTonalButton(text="CA", on_click=clearer, width=75),
+                ft.FilledTonalButton(text="/", on_click=actdiv, width=75),
+                ft.FilledTonalButton(text="*", on_click=actmult, width=75),
+            ],
+            alignment=ft.MainAxisAlignment.CENTER,
         ),
-    ft.Row(
-        [
-            ft.FilledTonalButton(text="=", on_click=None),
-        ],
-        alignment = ft.MainAxisAlignment.CENTER,
+        ft.Row(
+            [
+                ft.FilledTonalButton(text="=", on_click=igual, width=330),
+            ],
+            alignment=ft.MainAxisAlignment.CENTER,
         )
     )
+
 
 ft.app(target=main)
